69e0851c3dad602fbbca907307a13820a63322ce
MANOJ GOWDA AC <95601278+manojgowdaac@users.noreply.github.com>
2024-03-28 20:20:35 +0530

    Update terraForm.tf

--- projects/terraForm.tf
+++ projects/terraForm.tf
@@ -1,5 +1,16 @@
-hiii
-helooooi
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "iam-user"
+  secret_key = "iam-user"
+}
 
 
-a+b=c
+resource "aws_instance""myec2" {
+  ami             = "ami-007020fd9c84e18c7"
+  instance_type   = "t2.micro"
+  key_name        = "Devops1"
+  security_groups = ["default"]
+  tags = {
+    Name = "newce2"
+  }
+}
